714ce506dee401e1f2e63ca324f0317f58717f90
smertzani444 <mertzaste@icloud.com>
2025-04-16 20:49:32 +0300

    Task 1

--- src/functions.py
+++ src/functions.py
@@ -20,13 +20,3 @@ from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, root_mean_squared_error
 from sklearn.model_selection import train_test_split, cross_val_score
 
-def load_data(self, path):
-        if not os.path.isfile(path):
-            raise FileNotFoundError(f"The file at {path} was not found.")
-        if 'dev' in path:
-            print('Data for development:')
-        elif 'val' in path:
-            print('Data for evaluation:')
-        else:
-            raise ValueError("The path provided does not contain data for development nor for evaluation.")
-        return pd.read_csv(path)
